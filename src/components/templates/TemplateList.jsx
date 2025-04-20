a3cc029f8024637082f55aa88c32a5d5382087fd
antony190599 <antonycayo19@gmail.com>
2025-04-19 22:43:02 -0500

    icons

--- src/components/templates/TemplateList.jsx
+++ src/components/templates/TemplateList.jsx
@@ -261,31 +261,46 @@ const TemplateList = () => {
                           {new Date(template.createdAt).toLocaleDateString()}
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                          <button
-                            onClick={() => openPreviewModal(template)}
-                            className="text-gray-600 hover:text-gray-900 mr-4"
-                          >
-                            Ver
-                          </button>
-                          <Link 
-                            to={`/templates/edit/${template.id}`}
-                            className="text-blue-600 hover:text-blue-900 mr-4"
-                          >
-                            Editar
-                          </Link>
-                          <Link
-                            to={`/templates/send`}
-                            state={{ template: template }}
-                            className="text-green-600 hover:text-green-900 mr-4"
-                          >
-                            Enviar
-                          </Link>
-                          <button
-                            onClick={() => openDeleteConfirmation(template.id)}
-                            className="text-red-600 hover:text-red-900"
-                          >
-                            Eliminar
-                          </button>
+                          <div className="flex justify-end">
+                            <button
+                              onClick={() => openPreviewModal(template)}
+                              className="text-gray-600 hover:text-gray-900 mr-4"
+                              title="Ver"
+                            >
+                              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                              </svg>
+                            </button>
+                            <Link 
+                              to={`/templates/edit/${template.id}`}
+                              className="text-blue-600 hover:text-blue-900 mr-4"
+                              title="Editar"
+                            >
+                              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                              </svg>
+                            </Link>
+                            <Link
+                              to={`/templates/send`}
+                              state={{ template: template }}
+                              className="text-green-600 hover:text-green-900 mr-4"
+                              title="Enviar"
+                            >
+                              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+                              </svg>
+                            </Link>
+                            <button
+                              onClick={() => openDeleteConfirmation(template.id)}
+                              className="text-red-600 hover:text-red-900"
+                              title="Eliminar"
+                            >
+                              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                              </svg>
+                            </button>
+                          </div>
                         </td>
                       </tr>
                     ))}
@@ -347,9 +362,12 @@ const TemplateList = () => {
               <Link
                 to="/templates/send"
                 state={{ template: previewModal.template }}
-                className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
+                className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 flex items-center"
                 onClick={closePreviewModal}
               >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+                </svg>
                 Enviar
               </Link>
             </div>
